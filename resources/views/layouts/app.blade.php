9d5b9fc00bd3206c7ab56a24b457d1b6cb641010
vagrant <vagrant@homestead>
2018-01-14 16:41:35 +0000

    repaired helpsers function not fine error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,31 +1,34 @@
-!<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
-    </head>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
 
-    <body>
-        <div id="app" class="{{ route_class() }}-page">
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
 
-            @include('layouts._header')
+<body>
+    <div id="app" class="{{ route_class() }}-page">
 
-            <div class="container">
+        @include('layouts._header')
 
-                @yield('content')
+        <div class="container">
 
-            </div>
+            @yield('content')
 
-            @include('layouts._footer')
         </div>
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}"></script>
-    </body>
+        @include('layouts._footer')
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
 </html>
