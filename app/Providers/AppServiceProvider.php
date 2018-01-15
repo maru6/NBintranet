99f9602e666811401f81365e381bc1d12ea29998
vagrant <vagrant@homestead>
2018-01-15 16:44:41 +0000

    basic user profile editing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,8 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \Carbon\Carbon::setLocale('zh');
     }
+
 
     /**
      * Register any application services.
