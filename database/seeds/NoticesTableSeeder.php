dada74379ecdcdcbd736c4746a1f6977c6a32715
vagrant <vagrant@homestead>
2018-01-27 14:07:56 +0000

    Seeding Notices data

--- database/seeds/NoticesTableSeeder.php
+++ database/seeds/NoticesTableSeeder.php
@@ -2,19 +2,37 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Notice;
+use App\Models\User;
+use App\Models\Department;
 
 class NoticesTableSeeder extends Seeder
 {
     public function run()
     {
-        $notices = factory(Notice::class)->times(50)->make()->each(function ($notice, $index) {
-            if ($index == 0) {
-                // $notice->field = 'value';
-            }
+        // 所有用户 ID 数组，如：[1,2,3,4]
+        $user_ids = User::all()->pluck('id')->toArray();
+
+        // 所有分类 ID 数组，如：[1,2,3,4]
+        $department_ids = department::all()->pluck('id')->toArray();
+
+        // 获取 Faker 实例
+        $faker = app(Faker\Generator::class);
+
+        $notices = factory(Notice::class)
+                         ->times(50)
+                         ->make()
+                         ->each(function ($notice, $index)
+                            use($user_ids, $department_ids, $faker)
+        {
+            // 从用户 ID 数组中随机取出一个并赋值
+            $notice->user_id = $faker->randomElement($user_ids);
+
+            // 话题分类，同上
+            $notice->department_id = $faker->randomElement($department_ids);
+
         });
 
         Notice::insert($notices->toArray());
     }
 
 }
-
